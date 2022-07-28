5479d778b714782cae4807962e696c2fc5c3efc8
Neftali-Samarey <neftali.quintero@cix.csi.cuny.edu>
2022-07-27 20:16:49 -0400

    Initial Commit

---
+++ Avisa/AvisaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvisaApp.swift
+//  Avisa
+//
+//  Created by Neftali Samarey on 7/27/22.
+//
+
+import SwiftUI
+
+@main
+struct AvisaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
